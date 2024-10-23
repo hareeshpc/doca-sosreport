694e3594f68c331024a5b4af0e71784f49547f34
Hareesh Puthalath <hareeshp@nvidia.com>
2024-10-23 23:21:51 +0200

    [HBN] Add additional hbn related commands and log files.
    Signed-off-by: Hareesh Puthalath <hareeshp@nvidia.com>

--- sos/report/plugins/hbn.py
+++ sos/report/plugins/hbn.py
@@ -12,8 +12,9 @@ from sos.report.plugins import Plugin, IndependentPlugin
 
 class HBN(Plugin, IndependentPlugin):
 
-    short_desc = 'HBN (Host Based Network)'
+    short_desc = 'HBN (Host Based Networking)'
     plugin_name = "hbn"
+    vsctl = "ovs-vsctl"
     packages = ('hbn-repo',)
 
     def setup(self):
@@ -21,4 +22,20 @@ class HBN(Plugin, IndependentPlugin):
             '/etc/mellanox',
             '/var/log/doca/hbn',
             '/var/lib/hbn/etc/cumulus',
+            '/var/log/containers',
+            self.path_join('/var/log', 'sfc-install.log'),
+            self.path_join('/tmp', 'sf_devices'),
+            self.path_join('/tmp', 'sfr_devices'),
+            self.path_join('/tmp', 'sfc-activated'),
+        ])
+
+        self.add_journal(units="sfc")
+        self.add_journal(units="sfc-state-propagation")
+
+        self.add_cmd_output([
+            f"{self.vsctl} -t 5 list Open_vSwitch .",
+            f"{self.vsctl} -t 5 show",
+            "crictl ps -a",
+            "mlnx-sf -a show",
+            "mlnx-sf -a show --json --pretty",
         ])
